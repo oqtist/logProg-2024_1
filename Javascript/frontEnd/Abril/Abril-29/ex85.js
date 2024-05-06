b89158b0030cb7acd9b2dae5cf796d24b538023e
0qtist <77591116+oqtist@users.noreply.github.com>
2024-05-06 20:58:49 -0300

    Array
    Adicionados mais exs. de array

---
+++ Javascript/frontEnd/Abril/Abril-29/ex85.js
@@ -0,0 +1,13 @@
+let vetores = []
+let vetorDisplay = document.getElementById('vetorDisplay')
+
+for(i=0;i<51;i++) {
+    vetores.push(Math.floor(Math.random() * (201) ) - 100)
+}
+
+for(i=0;i<vetores.length;i++) {
+    vetorDisplay.innerHTML += (`Posição ${i}: ${vetores[i]}<br>`)
+    if(vetores[i] < 0) {
+        vetores.splice[i,1,0]
+    }
+}
--- Javascript/frontEnd/Abril/Abril-29/teste.js
+++ Javascript/frontEnd/Abril/Abril-29/teste.js
@@ -14,14 +14,14 @@ console.log(diasDaSemana);
 
 let numerosPares = []
 
-for(i=0;i<21;i++) {
-    if(i % 2 == 0) {
+for (i = 0; i < 21; i++) {
+    if (i % 2 == 0) {
         numerosPares.push(i)
     }
 }
 console.log(numerosPares);
 
-numerosPares.splice(2,1,numerosPares[6])
+numerosPares.splice(2, 1, numerosPares[6])
 console.log(numerosPares);
 
 //================================================================//
@@ -30,7 +30,7 @@ let frutas = ['banana', 'maçã', 'uva']
 frutas.push('morango')
 console.log(frutas);
 
-frutas.splice(1,1)
+frutas.splice(1, 1)
 console.log(frutas);
 
 //================================================================//
@@ -49,7 +49,7 @@ console.log(`${idades[0] + idades[1]}`)
 
 let soma = 0
 
-for(i = 0;i<idades.length;i++) {
+for (i = 0; i < idades.length; i++) {
     soma += idades[i]
 }
 
@@ -64,11 +64,11 @@ console.log(compras)
 compras.push('Ovos', 'Arroz')
 console.log(compras);
 
-compras.splice(2,1)
+compras.splice(2, 1)
 console.log(compras);
 
 let paoExiste
-if(compras.indexOf('Pão') != -1) {
+if (compras.indexOf('Pão') != -1) {
     paoExiste = true
 } else {
     paoExiste = false
@@ -79,20 +79,64 @@ console.log(paoExiste)
 
 let impares = []
 
-for(i=1;i<10;i+=2) {
+for (i = 1; i < 10; i += 2) {
     impares.push(i)
 }
 console.log(impares)
 
-for(i=0;i<impares.length;i++) {
+for (i = 0; i < impares.length; i++) {
     impares.splice(i, 1, (impares[i] * 2))
 }
 console.log(impares);
 
 //================================================================//
 
-let cidades = ['Paris', 'Londres', 'Nápoles']
+let cidades = ['o Cairo', 'Londres', 'Nápoles']
 
 console.log(`Eu adoraria visitar ${cidades[1]} e ${cidades[2]}.`);
+
+//================================================================//
+
+let arrayDisp = document.getElementById('arrayDisplay')
+let somaDisp = document.getElementById('somaValores')
+
+let array = []
+
+for (i = 0; i < 25; i++) {
+    array.push(parseInt(Math.ceil(Math.random() * 100)))
+}
+
+let n1 = -1, n2 = -1
+
+while (n1 < 0 || n1 > 24) {
+    n1 = Number(prompt('Digite a primeira posição (de 0 a 24) a ser encontrada:'))
+    if (n1 >= 0 && n1 <= 24) {
+        alert(`O valor da posição ${n1} é ${array[n1]}.`)
+    } else {
+        alert('Posição Inválida.')
+    }
+}
+
+while (n2 < 0 || n2 > 24) {
+    n2 = Number(prompt('Digite a segunda posição (de 0 a 24) a ser encontrada:'))
+    if (n2 >= 0 && n2 <= 24) {
+        alert(`O valor da posição ${n2} é ${array[n2]}.`)
+    } else {
+        alert('Posição Inválida.')
+    }
+}
+
+somaDisp.innerHTML = (`A soma dos valores nas posições ${n1} e ${n2} é ${array[n1] + array[n2]}`)
+
+let existe = Number(prompt('Digite um número para saber se ele existe no vetor:'))
+if (array.indexOf(existe) != -1) {
+    alert(`O valor está presente no array na posição ${(array.indexOf(existe))}.`)
+} else {
+    alert(`O valor não existe no array.`)
+}
+
+for (i = 0; i < array.length; i++) {
+    arrayDisp.innerHTML += (`Posição ${i}: ${array[i]}<br>`)
+}
 
 //================================================================//
